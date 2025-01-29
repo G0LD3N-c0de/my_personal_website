689598af205debe065636cbdb8d82a2dd444883a
SpaceMan28 <nicokazemi34@gmail.com>
2025-01-29 16:59:04 -0700

    style: fix styling issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css";
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <h1 className="title">Alakazemi Creations</h1>
     </header>
   );
