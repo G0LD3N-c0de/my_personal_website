d8a5dbfe6ca35b261dcb9b886d1bd472d258fdda
SpaceMan28 <nicokazemi34@gmail.com>
2025-02-26 11:38:25 -0700

    style: rework homepage

--- src/components/About.js
+++ src/components/About.js
@@ -5,27 +5,11 @@ export default function About() {
     <div className="about">
       <img src={self} className="about__image" alt="image of portfolio owner" />
       <div className="about__text">
-        <h3 className="about__text-title">About Me</h3>
-        <div className="about__text-paragraph">
-          Hey there! My name is Nicoli Kazemi, and I am a software developer.
-          Specialized in and passionate about web development and UI/UX design.
-          As an alumni of TripleTen Software School, I learned both the
-          front-end and back-end side of the development process. Always
-          interested in opportunites where I can improve my web development
-          skills.
-          <br></br>
-          <br></br>I am also an author with my first self-published book,
-          "Divergent Point" set to be published soon. Don't get too excited. It
-          will definately have that first book energy :D
-          <br></br>
-          <br></br>
-          Colorado resident and graduate of University of Colorado, Colorado
-          Springs. In my freetime, I like to read and play video games or go out
-          and explore Colorado's vast natural beauty.
-          <br></br>
-          <br></br>
-          Feel free to connect with me!
-        </div>
+        <h3 className="about__text-title">Nicoli Kazemi</h3>
+        <p className="about__text-description">
+          Web Developer, Author, Educator, Big Booty Latina Lover, World
+          Traveler
+        </p>
       </div>
     </div>
   );
